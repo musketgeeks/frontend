669bae6ab3a166d995b33a7274765bd3142681ac
Daniel Sousa <d.sousa@kigroup.de>
2022-10-19 21:42:25 +0100

    feat: tailwindcss configuration and basic test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 
+import '@styles/global.scss';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
